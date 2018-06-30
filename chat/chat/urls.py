0b14357266f0d9c9bde792428aceaa8b1d1eb646
plutasnyy <kamil.plucinski97@gmail.com>
2018-06-30 19:09:16 +0200

    created initial app

---
+++ chat/chat/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+urlpatterns = [
+    url(r'^chat/', include('chat_app.urls')),
+    url(r'^admin/', admin.site.urls),
+]
---
+++ chat/chat_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# chat/views.py
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'chat_app/index.html', {})
