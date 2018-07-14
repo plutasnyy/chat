8e808ac62370862846f371f1934bf2ba0b817128
plutasnyy <kamil.plucinski97@gmail.com>
2018-07-14 16:50:02 +0200

    added react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Room from "./Room";
+import NotFound from "./NotFound";
 
 class App extends Component {
   render() {
@@ -13,6 +16,12 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+            <BrowserRouter>
+            <Switch>
+            <Route exact path="/" component={Room} />
+            <Route component={NotFound} />
+            </Switch>
+            </BrowserRouter>
       </div>
     );
   }
